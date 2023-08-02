dc084101826f49d5187051e1a1c3590998f3a80c
German Diaz <germanrodrigo.2003@hotmail.com>
2023-08-01 21:16:35 -0500

    primera version del portafolio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'germanSiteWeb';
+  edad = '20';
+  email = 'germanrodrigo.2003@hotmail.com';
+  telefono = '3208715459';
+
+  enviarMensajeWhatsapp() {
+    const numeroWhatsApp = this.telefono;
+    const url = 'https://wa.me/' + numeroWhatsApp;
+    window.open(url, '_blank');
+  }
+
+  descargarCurriculum() {
+    const nombreArchivo = 'GermanDiazCurriculum.pdf';
+    const rutaArchivo = '../assets/' + nombreArchivo;
+    const link = document.createElement('a');
+    link.setAttribute('target', '_black');
+    link.setAttribute('href', rutaArchivo);
+    link.setAttribute('download', nombreArchivo);
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
 }
